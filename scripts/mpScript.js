84f70c6398b56485ebbdae81a71e14f83a82d1ac
Matei Cloteaux <matei.cloteaux@gmail.com>
2023-03-01 21:48:11 -0500

    Finish MP

--- scripts/mpScript.js
+++ scripts/mpScript.js
@@ -1,7 +1,7 @@
 //const header = document.getElementById("header_heading");
 const arcadeImg = document.getElementById("arcadeBG");
 const screenDiv = document.getElementById("screen");
-const buttOne = document.getElementById("buttOne");
+// const buttOne = document.getElementById("buttOne");
 const polybius = document.getElementById("polybiusImg");
 
 
@@ -22,7 +22,7 @@ function changeWidths(){
     const margins = 8.0; 
 
     changeElementSizeAndLocation(screenDiv, arcadeImg, 276.0/353.0, 353.0/arcadeBGW, 94.0/353.0, 182.0/353.0); // Resize screen div
-    changeElementSizeAndLocation(buttOne, arcadeImg, 23.0/29.0, 29.0/arcadeBGW, 127.0/29.0, 527.0/29.0); // Resize butt one
+    // changeElementSizeAndLocation(buttOne, arcadeImg, 23.0/29.0, 29.0/arcadeBGW, 127.0/29.0, 527.0/29.0); // Resize butt one
 
     for(let i=0; i<gameImgs.length - 1; i++){
         const imgsPerRow = 6;
@@ -33,8 +33,8 @@ function changeWidths(){
             (xStartOffset + col * (imgSqSize + margins))/imgSqSize, (yStartOffset + row * (imgSqSize + margins))/imgSqSize);
 
     }
-    changeElementSizeAndLocation(gameImgs[gameImgs.length-1], arcadeImg, 0.5, imgSqSize * 1.5/arcadeBGW, 
-        (xStartOffset + 2.3 * (imgSqSize + margins))/(imgSqSize * 1.5), (yStartOffset + 3.1 * (imgSqSize + margins))/(imgSqSize * 1.5));
+    changeElementSizeAndLocation(gameImgs[gameImgs.length-1], arcadeImg, 0.5, imgSqSize * 1.8/arcadeBGW, 
+        (xStartOffset + 2.165 * (imgSqSize + margins))/(imgSqSize * 1.8), (yStartOffset + 3.05 * (imgSqSize + margins))/(imgSqSize * 1.8));
 
     // Polybius
     changeElementSizeAndLocation(polybius, arcadeImg, 750.0/650.0, 85.0/arcadeBGW, (xStartOffset + 113.0)/85.0, (yStartOffset + 55.0)/85.0);
